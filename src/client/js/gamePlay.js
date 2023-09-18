7a2f8e3ab6e21c5f613c44cc08d374cc3c4e8a81
kangmin01 <minheek99@gmail.com>
2023-09-19 03:00:10 +0900

    프리셀과 컬럼 간 카드 이동 기능 추가

--- src/client/js/gamePlay.js
+++ src/client/js/gamePlay.js
@@ -10,7 +10,6 @@ const FREE = [null, null, null, null];
 const HOME = [null, null, null, null];
 const COLUMN = [[], [], [], [], [], [], [], []]; // 현재 카드
 const MOVABLE_COLUMN = [[], [], [], [], [], [], [], []]; // 드래그 가능한 카드
-const MOVABLE_CARDS = 5;
 
 // 현재 테이블의 카드 정보를 COLUMN배열에 추가
 const currentColumns = () => {
@@ -52,20 +51,32 @@ const movableCurrentColumns = (col) => {
 movableCurrentColumns(COLUMN);
 
 // drag ui
-let draggedCard;
+let targetImg;
+let draggedCard, draggedFreeCellCard;
 let originalColumn, originalColumnIdx;
+let targetColumn, targetColumnLastCardIdx, targetColumnLastCard;
+let isValid;
+let targetFreeCell, targetFreeCellIdx;
 
-columns.forEach((column, idx) => {
+for (let column of columns) {
   column.addEventListener("dragstart", (e) => {
-    const targetImg = e.target;
+    targetImg = e.target;
     draggedCard = e.target.parentNode;
     originalColumn = e.currentTarget;
     originalColumnIdx = Array.from(columns).indexOf(originalColumn);
-    e.dataTransfer.setDragImage(
-      targetImg,
-      targetImg.clientWidth / 2,
-      targetImg.clientHeight / 2
-    );
+    if (
+      MOVABLE_COLUMN[originalColumnIdx].some(
+        (card) => card.id == draggedCard.dataset.id
+      )
+    ) {
+      e.dataTransfer.setDragImage(
+        targetImg,
+        targetImg.clientWidth / 2,
+        targetImg.clientHeight / 2
+      );
+    } else {
+      e.preventDefault();
+    }
   });
 
   column.addEventListener("dragover", (e) => {
@@ -73,19 +84,119 @@ columns.forEach((column, idx) => {
   });
 
   column.addEventListener("drop", (e) => {
-    if (draggedCard) {
-      e.preventDefault();
-      const targetColumn = e.currentTarget;
-      const cardId = Number(draggedCard.dataset.id);
+    e.preventDefault();
+    targetColumn = e.currentTarget;
+    targetColumnLastCardIdx = Number(targetColumn.id[6]);
+    targetColumnLastCard =
+      COLUMN[targetColumnLastCardIdx][
+        COLUMN[targetColumnLastCardIdx].length - 1
+      ];
 
-      if (MOVABLE_COLUMN[originalColumnIdx].some((card) => card.id == cardId)) {
-        console.log("드롭 가능");
+    // 컬럼 -> 컬럼
+    if (draggedCard) {
+      if (COLUMN[targetColumnLastCardIdx].length === 0) {
         targetColumn.appendChild(draggedCard);
         draggedCard = null;
         currentColumns();
         movableCurrentColumns(COLUMN);
-      } else {
-        console.log("드롭 불가");
+        return;
+      }
+
+      isValid = isValidFn(targetColumnLastCard, draggedCard.dataset);
+      if (isValid) {
+        isValid = false;
+        targetColumn.appendChild(draggedCard);
+        draggedCard = null;
+        currentColumns();
+        movableCurrentColumns(COLUMN);
+      }
+    }
+
+    // 프리셀 -> 컬럼
+    if (draggedFreeCellCard) {
+      if (COLUMN[targetColumnLastCardIdx].length === 0) {
+        targetColumn.appendChild(draggedFreeCellCard);
+        draggedFreeCellCard = null;
+        currentColumns();
+        movableCurrentColumns(COLUMN);
+        return;
+      }
+
+      if (
+        targetColumnLastCard.color !== draggedFreeCellCard.dataset.color &&
+        Number(targetColumnLastCard.value) ===
+          Number(draggedFreeCellCard.dataset.value) + 1
+      ) {
+        targetColumn.appendChild(draggedFreeCellCard);
+        FREE[originalColumnIdx] = null;
+        draggedFreeCellCard = null;
+        currentColumns();
+        movableCurrentColumns(COLUMN);
+      }
+    }
+  });
+}
+
+const isValidFn = (target, dragged) => {
+  if (
+    MOVABLE_COLUMN[originalColumnIdx].some((card) => card.id == dragged.id) &&
+    target.color !== dragged.color &&
+    Number(target.value) === Number(dragged.value) + 1
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+freeCells.forEach((freeCell) => {
+  freeCell.addEventListener("dragstart", (e) => {
+    targetImg = e.target;
+    originalColumn = e.currentTarget;
+    originalColumnIdx = Number(e.currentTarget.id[4]);
+
+    // 프리셀에 카드가 있을 경우만 드래그를 가능하게 한다.
+    if (FREE[originalColumnIdx] !== null) {
+      draggedFreeCellCard = e.target.parentNode;
+      e.dataTransfer.setDragImage(
+        targetImg,
+        targetImg.clientWidth / 2,
+        targetImg.clientHeight / 2
+      );
+    } else {
+      e.preventDefault();
+    }
+  });
+
+  freeCell.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  freeCell.addEventListener("drop", (e) => {
+    e.preventDefault();
+    // 컬럼 -> 프리셀
+    if (draggedCard) {
+      const cardId = Number(draggedCard.dataset.id);
+
+      if (MOVABLE_COLUMN[originalColumnIdx].some((card) => card.id == cardId)) {
+        e.target.appendChild(draggedCard);
+        FREE[Number(e.target.id[4])] = draggedCard.dataset;
+        draggedCard = null;
+        currentColumns();
+        movableCurrentColumns(COLUMN);
+      }
+    }
+
+    // 프리셀 -> 프리셀
+    if (draggedFreeCellCard) {
+      targetFreeCell = e.currentTarget;
+      targetFreeCellIdx = Number(e.currentTarget.id[4]);
+
+      if (FREE[targetFreeCellIdx] === null) {
+        targetFreeCell.appendChild(draggedFreeCellCard);
+        FREE[targetFreeCellIdx] = draggedFreeCellCard.dataset;
+        FREE[originalColumnIdx] = null;
+        draggedFreeCellCard = null;
       }
     }
   });
