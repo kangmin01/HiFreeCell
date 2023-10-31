b0c9a92d7afe3c9a77f923ec00bc7478a4cfc5c7
kangmin01 <minheek99@gmail.com>
2023-10-31 20:48:22 +0900

    Fix: 게임 라우트 수정

--- src/routers/gameRouter.js
+++ src/routers/gameRouter.js
@@ -4,7 +4,7 @@ import { games, playGame } from "../controllers/gameController";
 
 const gameRouter = express.Router();
 
-gameRouter.get("/", games);
+gameRouter.route("/").all(userOnlyMiddleware).get(games);
 gameRouter.route("/:id").all(userOnlyMiddleware).get(playGame);
 
 export default gameRouter;
